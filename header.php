acbb40cda748ef7b27a1a42b7a7d52188a9922cc
chamberezigbo <chamberezigbo@gmail.com>
2023-08-14 11:47:56 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,85 @@
+     <?php
+     function isActive($page)
+     {
+          $currentURI = trim(str_replace('faster-logistics/', '', $_SERVER['REQUEST_URI']), '/');
+          $isActive = ($currentURI == $page) ? 'active' : '';
+          return $isActive;
+     }
+
+     ?>
+     <!DOCTYPE html>
+     <html lang="en">
+
+     <head>
+          <meta charset="utf-8" />
+          <title>FASTER - Logistics Company</title>
+          <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+          <meta content="Free HTML Templates" name="keywords" />
+          <meta content="Free HTML Templates" name="description" />
+
+          <!-- Favicon -->
+          <link href="img/favicon.ico" rel="icon" />
+          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqhmdN7U9cLCvALzm7jd6o9LT8fa-SFqE&libraries=geometry,places"></script>
+
+          <!-- Google Web Fonts -->
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
+          <!-- Font Awesome -->
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
+
+          <!-- Libraries Stylesheet -->
+          <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+
+          <!-- Customized Bootstrap Stylesheet -->
+          <link href="css/style.css" rel="stylesheet" />
+
+          <!-- alert -->
+          <script src="https://common.olemiss.edu/_js/sweet-alert/sweet-alert.min.js"></script>
+          <link rel="stylesheet" type="text/css" href="https://common.olemiss.edu/_js/sweet-alert/sweet-alert.css">
+     </head>
+
+     <body>
+          <!-- Topbar Start -->
+          <div class="container-fluid bg-dark">
+               <div class="row py-2 px-lg-5">
+                    <div class="col-lg-6 text-center text-lg-left mb-2 mb-lg-0">
+                         <div class="d-inline-flex align-items-center text-white">
+                              <small><i class="fa fa-phone-alt mr-2"></i>+131 5325 009</small>
+                              <small class="px-3">|</small>
+                              <small><i class="fa fa-envelope mr-2"></i>info@example.com</small>
+                         </div>
+                    </div>
+                    <div class="col-lg-6 text-center text-lg-right">
+                         <div class="d-inline-flex align-items-center">
+                              <a href="#" class="" id="google_translate_element"></a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+          <!-- Topbar End -->
+
+          <!-- Navbar Start -->
+          <div class="container-fluid p-0">
+               <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-lg-5">
+                    <a href="index.php" class="navbar-brand ml-lg-3">
+                         <h1 class="m-0 display-5 text-uppercase text-primary">
+                              <i class="fa fa-plane mr-2"></i>Faster Express
+                         </h1>
+                    </a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                         <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
+                         <div class="navbar-nav m-auto py-0">
+                              <a href="index.php" class="nav-item nav-link <?php echo isActive('index.php'); ?>">Home</a>
+                              <a href="about.php" class="nav-item nav-link <?php echo isActive('about.php'); ?>">About</a>
+                              <a href="service.php" class="nav-item nav-link <?php echo isActive('service.php'); ?>">Service</a>
+                              <a href="price.php" class="nav-item nav-link <?php echo isActive('price.php'); ?>">Price</a>
+                              <a href="contact.php" class="nav-item nav-link <?php echo isActive('contact.php'); ?>">Contact</a>
+                         </div>
+                         <a href="price.php" class="btn btn-primary py-2 px-4 d-none d-lg-block">Get A Quote</a>
+                    </div>
+               </nav>
+          </div>
+          <!-- Navbar End -->
---
+++ price.php
@@ -0,0 +1,189 @@
+<?php
+include "header.php";
+?>
+
+
+<!-- Header Start -->
+<div class="jumbotron jumbotron-fluid mb-5">
+     <div class="container text-center py-5">
+          <h1 class="text-white display-3">Price</h1>
+          <div class="d-inline-flex align-items-center text-white">
+               <p class="m-0"><a class="text-white" href="">Home</a></p>
+               <i class="fa fa-circle px-3"></i>
+               <p class="m-0">Price</p>
+          </div>
+     </div>
+</div>
+<!-- Header End -->
+
+
+<!-- Pricing Plan Start -->
+<div class="container-fluid pt-5">
+     <div class="container">
+          <div class="text-center pb-2">
+               <h6 class="text-primary text-uppercase font-weight-bold">Pricing Plan</h6>
+               <h1 class="mb-4">Affordable Pricing Packages</h1>
+          </div>
+          <div class="row">
+               <div class="col-md-4 mb-5">
+                    <div class="bg-secondary">
+                         <div class="text-center p-4">
+                              <h1 class="display-4 mb-0">
+                                   <small class="align-top text-muted font-weight-medium" style="
+											font-size: 22px;
+											line-height: 45px;
+										">$</small>49<small class="align-bottom text-muted font-weight-medium" style="
+											font-size: 16px;
+											line-height: 40px;
+										">/Mo</small>
+                              </h1>
+                         </div>
+                         <div class="bg-primary text-center p-4">
+                              <h3 class="m-0">Basic</h3>
+                         </div>
+                         <div class="d-flex flex-column align-items-center py-4">
+                              <p>Cutting-edge Technology</p>
+                              <p>Customer-Centric Approach:</p>
+                              <p>Warehousing</p>
+                              <p>Express Shipping:</p>
+                              <a href="about.php" class="btn btn-primary py-2 px-4 my-2">Order Now</a>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 mb-5">
+                    <div class="bg-secondary">
+                         <div class="text-center p-4">
+                              <h1 class="display-4 mb-0">
+                                   <small class="align-top text-muted font-weight-medium" style="
+											font-size: 22px;
+											line-height: 45px;
+										">$</small>99<small class="align-bottom text-muted font-weight-medium" style="
+											font-size: 16px;
+											line-height: 40px;
+										">/Mo</small>
+                              </h1>
+                         </div>
+                         <div class="bg-primary text-center p-4">
+                              <h3 class="m-0">Premium</h3>
+                         </div>
+                         <div class="d-flex flex-column align-items-center py-4">
+                              <p>Cutting-edge Technology</p>
+                              <p>Customer-Centric Approach:</p>
+                              <p>Warehousing</p>
+                              <p>Express Shipping:</p>
+                              <a href="about.php" class="btn btn-primary py-2 px-4 my-2">Order Now</a>
+                         </div>
+                    </div>
+               </div>
+               <div class="col-md-4 mb-5">
+                    <div class="bg-secondary">
+                         <div class="text-center p-4">
+                              <h1 class="display-4 mb-0">
+                                   <small class="align-top text-muted font-weight-medium" style="
+											font-size: 22px;
+											line-height: 45px;
+										">$</small>149<small class="align-bottom text-muted font-weight-medium" style="
+											font-size: 16px;
+											line-height: 40px;
+										">/Mo</small>
+                              </h1>
+                         </div>
+                         <div class="bg-primary text-center p-4">
+                              <h3 class="m-0">Business</h3>
+                         </div>
+                         <div class="d-flex flex-column align-items-center py-4">
+                              <p>Cutting-edge Technology</p>
+                              <p>Customer-Centric Approach:</p>
+                              <p>Warehousing</p>
+                              <p>Express Shipping:</p>
+                              <a href="about.php" class="btn btn-primary py-2 px-4 my-2">Order Now</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
+<!-- Pricing Plan End -->
+
+
+<!-- Testimonial Start -->
+<div class="container-fluid py-5">
+     <div class="container">
+          <div class="text-center pb-2">
+               <h6 class="text-primary text-uppercase font-weight-bold">Testimonial</h6>
+               <h1 class="mb-4">Our Clients Say</h1>
+          </div>
+          <div class="owl-carousel testimonial-carousel">
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-1.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   Alex Johnson
+                              </h6>
+                              <small>- SwiftTech Industries</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Fast Express Logistics has truly transformed the way we manage our shipping needs.
+                         Their reliable services and efficient solutions have saved us both time and resources.
+                         From the moment we engaged with them, we knew we had found a partner we could trust.
+                    </p>
+               </div>
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-2.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   Sofia García
+                              </h6>
+                              <small>- Nature's Harvest</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Exceptional service and seamless deliveries - that's what Fast Express Logistics delivers every time.
+                         Their attention to detail and commitment to meeting deadlines have consistently exceeded our expectations.
+                         It's been a pleasure working with a team that values our business as much as we do.
+                    </p>
+               </div>
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-3.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   James Anderson
+                              </h6>
+                              <small>- TechLink Innovations</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Fast Express Logistics is more than a logistics provider; they're a strategic partner. Their tailored solutions have optimized our supply chain,
+                         enabling us to focus on our core business. The team's expertise and dedication have contributed to our success in more ways than one.
+                    </p>
+               </div>
+               <div class="position-relative bg-secondary p-4">
+                    <i class="fa fa-3x fa-quote-right text-primary position-absolute" style="top: -6px; right: 0"></i>
+                    <div class="d-flex align-items-center mb-3">
+                         <img class="img-fluid rounded-circle" src="img/testimonial-4.jpg" style="width: 60px; height: 60px" alt="" />
+                         <div class="ml-3">
+                              <h6 class="font-weight-semi-bold m-0">
+                                   Lara Müller
+                              </h6>
+                              <small>- Trendy Treasures</small>
+                         </div>
+                    </div>
+                    <p class="m-0">
+                         Reliability, professionalism, and speed - that's what Fast Express Logistics represents. Their end-to-end logistics support has been instrumental in our growth journey. As a customer-oriented company,
+                         they've consistently gone above and beyond to ensure our shipments arrive on time and in perfect condition.
+                    </p>
+               </div>
+          </div>
+     </div>
+</div>
+<!-- Testimonial End -->
+
+<?php
+include "footer.php";
